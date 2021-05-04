7529b29f4edccf85aa2c73918a8164a869b592f0
John Manack <jmnck28@gmail.com>
2021-05-04 16:40:32 -0400

    updates exercies-1

--- exercise-1.js
+++ exercise-1.js
@@ -1,20 +1,15 @@
 'use strict';
 
-const {performance} = require('perf_hooks');
-
 const nemo = ['nemo'];
 
 const large = new Array(10000).fill('nemo')
 
 function findNemo(array) {
-    let t0 = performance.now();
     for (let i = 0; i < array.length; i++) {
         if (array[i] === 'nemo') {
             console.log('Found Nemo!')
         }
     }
-    let t1 = performance.now();
-    console.log('Call to find Nemo took ' + (t1 - t0) + ' milliseconds');
 }
 
-findNemo(nemo);
+findNemo(large); // 0(n) --> Linear Time
